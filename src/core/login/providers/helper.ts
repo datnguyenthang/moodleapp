413b848842c875559c7dd6bbc07e8b3d86e5e4df
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2020-07-29 15:49:51 +0700

    add splash and remove slide before login

--- src/core/login/providers/helper.ts
+++ src/core/login/providers/helper.ts
@@ -504,7 +504,7 @@ export class CoreLoginHelperProvider {
             const url = typeof CoreConfigConstants.siteurl == 'string' ?
                 CoreConfigConstants.siteurl : CoreConfigConstants.siteurl[0].url;
 
-            pageName = 'CoreLoginIntroductionPage';
+            pageName = 'CoreLoginCredentialsPage';
             params = { siteUrl: url };
         } else {
             pageName = 'CoreLoginSitePage';
